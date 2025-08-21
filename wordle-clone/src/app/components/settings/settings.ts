604ee4196b0366cb654e82def6bfb68b25e79bd4
Thomas Hawley <thawley22@turing.edu>
2025-08-21 09:33:46 -0600

    Move setting logic from start page to it's own component for re-usability

---
+++ wordle-clone/src/app/components/settings/settings.ts
@@ -0,0 +1,66 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import GameSettings from '../../models/GameSettings';
+import { MusicService } from '../../music.service';
+import { SettingsService } from '../../settings.service';
+
+@Component({
+  selector: 'app-settings',
+  standalone: true,
+  imports: [ReactiveFormsModule, CommonModule],
+  templateUrl: './settings.html',
+  styleUrl: './settings.css'
+})
+export class SettingsComponent implements OnInit {
+  @Input() currentSettings!: GameSettings;          
+  @Output() save = new EventEmitter<GameSettings>(); 
+  @Output() close = new EventEmitter<void>();        
+
+  settingsForm!: FormGroup;
+
+  ngOnInit(): void {
+    this.setForm();
+  }
+
+  constructor(private musicService: MusicService, private settingService: SettingsService){}
+
+  private setForm() {
+    this.settingsForm = new FormGroup({
+      volume: new FormControl(this.currentSettings.volume),
+      darkMode: new FormControl(this.currentSettings.darkMode),
+      lettersPerWord: new FormControl(this.currentSettings.lettersPerWord, [
+        Validators.required,
+        Validators.min(3),
+        Validators.max(8)
+      ])
+    });
+  }
+
+  onSubmit() {
+    if (this.settingsForm.valid) {
+      this.handleSave(this.settingsForm.value)
+      this.save.emit(this.settingsForm.value);
+    }
+  }
+
+  handleSave(newSettings: GameSettings) {
+    this.settingService.saveSettings(newSettings);
+    this.musicService.setVolume(newSettings.volume);
+    this.currentSettings = newSettings;
+    this.toggleDarkMode();
+    
+  }
+
+  toggleDarkMode() {
+    this.currentSettings.darkMode
+      ? document.body.classList.add('dark-mode')
+      : document.body.classList.remove('dark-mode');
+  }
+
+  sameSettings() {
+    return this.currentSettings.darkMode === this.settingsForm.value.darkMode 
+      && this.currentSettings.volume === this.settingsForm.value.volume 
+      && this.currentSettings.lettersPerWord === this.settingsForm.value.lettersPerWord;
+  }
+}
--- wordle-clone/src/app/start-page/start-page.ts
+++ wordle-clone/src/app/start-page/start-page.ts
@@ -1,86 +1,52 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { WordService } from '../word.service';
-import { SettingsService } from '../settings.service';
-import GameSettings from '../models/GameSettings';
-import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { MusicService } from '../music.service';
+import { SettingsComponent } from '../components/settings/settings';
 import { LeaderBoard } from '../components/leaderboard/leaderboard';
+import GameSettings from '../models/GameSettings';
+import { SettingsService } from '../settings.service';
+import { Router } from '@angular/router';
+import { MusicService } from '../music.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-start-page',
-  imports: [ReactiveFormsModule, CommonModule, LeaderBoard],
+  imports: [CommonModule, LeaderBoard, SettingsComponent],
   templateUrl: './start-page.html',
   styleUrl: './start-page.css'
 })
-export class StartPage implements OnInit{
+export class StartPage {
+  currentSettings!: GameSettings;
+  settingsPage = false;
+  showLeaderBoard = false;
 
-  currentSettings!: GameSettings
-  settingsForm!: FormGroup;
-  settingsPage: boolean = false
-  showLeaderBoard: boolean = false
-
-
-  constructor(private wordService: WordService, private settingService: SettingsService, private router: Router, private musicService: MusicService) {
-    
-    this.setSettingsForm()
-  }
-  ngOnInit(): void {
-    this.currentSettings.darkMode ? document.body.classList.add('dark-mode') : document.body.classList.remove('dark-mode')
-  }
-
-  toggleDarkMode() {
-    this.currentSettings.darkMode ? document.body.classList.add('dark-mode') : document.body.classList.remove('dark-mode')
-  }
-  
-  onSubmit() {
-    this.settingService.saveSettings(this.settingsForm.value)
-    this.musicService.setVolume(this.settingService.getSettings().volume)
-    // this.musicService.play()
-    this.currentSettings = this.settingService.getSettings()
-    this.toggleDarkMode()
-    this.setSettingsForm()
-  }
-  
-  setSettingsForm() {
-    this.currentSettings = this.settingService.getSettings()
-    this.settingsForm = new FormGroup({
-      volume: new FormControl(this.currentSettings.volume),
-      darkMode: new FormControl(this.currentSettings.darkMode),
-      lettersPerWord: new FormControl(this.currentSettings.lettersPerWord, [
-        Validators.required,
-        Validators.minLength(3),
-        Validators.maxLength(8)
-      ])
-    })
+  constructor(
+    private settingService: SettingsService,
+    private router: Router,
+    private musicService: MusicService
+  ) {
+    this.currentSettings = this.settingService.getSettings();
   }
 
   startGame() {
     this.router.navigate(['game']);
   }
 
-  sameSettings() {
-    console.log('current settings',this.currentSettings)
-    console.log('form settings:', this.settingsForm.value)
-    return this.currentSettings.darkMode === this.settingsForm.value.darkMode 
-    && this.currentSettings.volume === this.settingsForm.value.volume 
-    && this.currentSettings.lettersPerWord === this.settingsForm.value.lettersPerWord
-  }
-
   showSettingsPage() {
-    // this.musicService.play()
-    this.settingsPage = true
+    this.settingsPage = true;
   }
 
-  hideSettingsPage() {
-    this.settingsPage = false
+  handleCloseSettings() {
+    this.settingsPage = false;
   }
 
   openLeaderBoard() {
     this.showLeaderBoard = true;
   }
+
   handleCloseLeaderBoard(closed: boolean) {
     this.showLeaderBoard = !closed;
+  }
+
+  importNewSettings(newSetting: GameSettings) {
+    this.currentSettings = newSetting
   }
 }
